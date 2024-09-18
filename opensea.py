b49733258d457e622bebec46f4f83f220fe6131c
webcoderz <19884161+webcoderz@users.noreply.github.com>
2024-09-18 14:45:44 -0400

    updating to working condition.. add api key and change collectioninfo dict key mapping, fixed random user agent generation (#53)
    * updating to working condition.. add api key and change collectioninfo dict mapping

--- opensea.py
+++ opensea.py
@@ -34,12 +34,13 @@ user_agent = user_agent_rotator.get_random_user_agent()
 # Headers for the request. Currently this is generating random user agents
 # Use a custom header version here -> https://www.whatismybrowser.com/guides/the-latest-user-agent/
 headers = {
-    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+    'User-Agent': user_agent
+    'X-API-KEY': os.environ['OPENSEA_API_KEY']
 }
 
 # Get information regarding collection
 
-collection = requests.get(f"http://api.opensea.io/api/v1/collection/{CollectionName}?format=json")
+collection = requests.get(f"http://api.opensea.io/api/v1/collection/{CollectionName}?format=json",headers=headers)
 
 if collection.status_code == 429:
     print("Server returned HTTP 429. Request was throttled. Please try again in about 5 minutes.")
